cef559504991dc51e808c1867d3c470a54607ae3
BoroaDev7 <jmborjas@unitec.edu>
2022-01-28 15:51:45 -0600

    Segundo avance

--- src/laboratorio28enero/Animales.java
+++ src/laboratorio28enero/Animales.java
@@ -1,13 +1,103 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package laboratorio28enero;
+
+import java.util.ArrayList;
 
 /**
  *
- * @author famil
+ * @author Juan Manuel Borjas Figueroa
  */
 public class Animales {
+    private String nombrecien;
+    private String nombre;
+    private String habitat;
+    private String alimentacion;
+    private String desrasgos;
+    private String disgeo;
+    private int vida;
+ 
+    
+     public Animales(){
+         
+     }
+     
+      public Animales(String nombrecien,String nombre, String habitat, String alimentacion, String desrasgos, String disgeo, int vida){
+          this.nombrecien=nombrecien;
+          this.nombre=nombre;
+          this.habitat=habitat;
+          this.alimentacion=alimentacion;
+          this.desrasgos=desrasgos;
+          this.disgeo=disgeo;
+          this.vida=vida;
+  
+       }
+      
+      
+      public void setNombrecien(String nombrecien){
+          this.nombrecien=nombrecien;
+        
+      }
+    
+      public String getNombrecien(){
+          return nombrecien;
+      }
+      public String setNombre(String nombre){
+          this.nombre=nombre;
+          return null;
+      }
+      public String getNombre(){
+          return nombre;
+      }
+      public void setHabitat(String habitat){
+          this.habitat=habitat;
+         
+      }
+      public String getHabitat(){
+          return habitat;
+         
+      }
+      public void  setAlimentacion(String alimentacion){
+          this.alimentacion=alimentacion;
+      }
+      public String getAlimentacion(){
+          return alimentacion;
+         
+      }
+      public void setDesrasgos(String desrasgos){
+          this.desrasgos=desrasgos;
+          
+      }
+      public String getDesrasgos(){
+          return desrasgos;
+          
+      }
+     
+      public void setDisgeo(String disgeo){
+          this.disgeo=disgeo;
+          
+      }
+
+      public String getDisgeo(){
+          return disgeo;
+      }
+      
+      public void setVida(int vida){
+          this.vida=vida;
+      }
+      public int getVida(){
+          return vida;
+      }
+    
+      
+      @Override
+    public String toString() {
+    return "Nombre cientifico="+nombrecien+" Nombre="+nombre+" Habitat="+habitat+" Alimentacion="+alimentacion +" Descripcion de rasgos="+desrasgos+" Distribucion geografica="+disgeo+" Vida="+vida;
+   
+    }
+        
+    
+    
     
 }
+
+ 
--- src/laboratorio28enero/Laboratorio28enero.java
+++ src/laboratorio28enero/Laboratorio28enero.java
@@ -1,20 +1,184 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package laboratorio28enero;
+
+import java.util.ArrayList;
+import java.util.Scanner;
 
 /**
  *
- * @author famil
+ * @author Juan Manuel Borjas Figueroa
  */
 public class Laboratorio28enero {
+  public static ArrayList<Animales> lista=new ArrayList();
+  public static Scanner leer= new Scanner(System.in);
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+     
+        opciones ( menu());
+     
+     
+     
     }
     
+    
+    public static int menu(){
+        System.out.println("");
+        System.out.println("");
+        System.out.println("0. Salir");
+        System.out.println("1. Registrar animales\n" +
+                            "2. Listar animales\n" +
+                            "3. Editar animales\n" +
+                            "4. Eliminar alumno\n" +
+                            "5. Alimentar animales\n" +
+                            "Ingrese la opcion: ");
+        return leer.nextInt();
+    }
+     public static void opciones(int opcion){
+         if(opcion == 0)
+            System.exit(0);
+        else if(opcion == 1)
+            registrar();
+        else if(opcion == 2)
+            listar();
+        else if(opcion == 3)
+            editar();
+        else if(opcion == 4)
+            eliminar();
+        else if(opcion == 5)
+            alimentar();
+         
+         }
+     
+     public static void registrar(){
+         System.out.print("Nombre Cientifico: ");
+        String nombrecien = leer.nextLine();
+        nombrecien = leer.nextLine();
+        System.out.print("Nombre Comun: ");
+        String nombre = leer.nextLine();
+        System.out.print("Habitat: ");
+        String habitat = leer.nextLine();
+        System.out.print("Alimentacion: ");
+        String alimentacion = leer.nextLine();
+        System.out.print("Descripcion de rasgos: ");
+        String desrasgos = leer.nextLine();
+        System.out.print("Distribucion Geografica: ");
+        String disgeo = leer.nextLine();
+        System.out.print("Vida: ");
+        int vida=leer.nextInt();
+        while(vida==0){
+            System.out.println("La vida no puede ser 0\n");
+            System.out.print("Vida: ");
+            vida=leer.nextInt();   
+        }
+        lista.add(new Animales(nombrecien, nombre, habitat, alimentacion, desrasgos, disgeo, vida));
+     }
+ 
+     public static void listar(){
+        
+         System.out.println("Ingrese la opcion que quiere imprimir");
+         int opcion=leer.nextInt();
+         System.out.println("1.Imrprimir por posicion de la lista"+
+                              "2.Imprimir lista completa" +
+                                "3.Imprimir por nombre cientifico");
+         
+          if(opcion == 1){
+              System.out.println("Ingrese la posicion");
+          int pos=leer.nextInt();
+            imprimirpos(pos);
+            }
+            else if(opcion == 2)
+            imprimircomp();
+            else if(opcion == 3)
+             System.out.println("Ingrese el Nombre Cientifico del animal");
+            String imp=leer.nextLine();
+             int indice=lista.indexOf(imp);
+            imprimirnomb(indice,imp);
+            
+         } 
+     public static void imprimirpos(int pos){
+         System.out.println(lista.get(pos));
+     }
+     public static void imprimircomp(){
+         for (Animales animales : lista) {
+            System.out.println(animales.toString());
+        }
+     }
+     public static void imprimirnomb(int indice,String imp){
+         System.out.println(lista.indexOf(imp));
+         
+     }
+     
+     public static void editar(){
+         System.out.println("Ingrese el nombre cienfitico del animal");
+         String imp=leer.nextLine();
+             int indice=lista.indexOf(imp);
+             System.out.println("Ingrese la opcion que desea editar" );
+             int opcion=leer.nextInt();
+             System.out.println(    "1.Editar un Atributo"+
+                                    "2.Editar todo");
+             if(opcion==1)
+                 editaruno(indice);
+             else if(opcion==2)
+                 editartodos(indice);
+     }
+      
+     
+     public static void editaruno(int indice){
+        System.out.println("1. Nombre Cientifico");
+        System.out.println("2. Nombre Comun");
+        System.out.println("3. Habitat");
+        System.out.println("4. Alimentacion");
+        System.out.println("5. Descripcion de Rasgos");
+        System.out.println("6. Distribucion Geografica");
+        System.out.println("7. Vida");
+        System.out.print("Ingrese la opcion: ");
+        int opcion = leer.nextInt();
+        if(opcion == 1){
+            System.out.print("Nombre Cientifico: ");
+            lista.get(indice).setNombrecien(leer.nextLine());
+        }else if(opcion == 2){
+            System.out.print("Nombre: ");
+            lista.get(indice).setNombre(leer.nextLine());
+        }else if(opcion == 3){
+            System.out.print("Habitat: ");
+            lista.get(indice).setHabitat(leer.nextLine());
+        }else if(opcion == 4){
+            System.out.println("Alimentacion: ");
+            lista.get(indice).setAlimentacion(leer.next());
+        }else if(opcion == 5){
+            System.out.print("Descripcion de Rasgos: ");
+            lista.get(indice).setDesrasgos(leer.nextLine());
+        }else if(opcion == 6){
+            System.out.print("Distribucion Geografica: ");
+            lista.get(indice).setDisgeo(leer.nextLine());
+        }else if(opcion == 7){
+            System.out.println("Vida: ");
+            lista.get(indice).setVida(leer.nextInt());
+        }
+    }
+     public static void editartodos(int indice){
+         System.out.print("Nombre Cientifico: ");
+            lista.get(indice).setNombrecien(leer.nextLine());
+        System.out.print("Nombre: ");
+            lista.get(indice).setNombre(leer.nextLine());
+        System.out.print("Habitat: ");
+            lista.get(indice).setHabitat(leer.nextLine());
+        System.out.println("Alimentacion: ");
+            lista.get(indice).setAlimentacion(leer.next());
+       System.out.print("Descripcion de Rasgos: ");
+            lista.get(indice).setDesrasgos(leer.nextLine());
+        System.out.print("Distribucion Geografica: ");
+            lista.get(indice).setDisgeo(leer.nextLine());
+       System.out.println("Vida: ");
+            lista.get(indice).setVida(leer.nextInt());
+    }
+     
+     
+    public static void eliminar(){
+        
+    }
+     
+     
+   
+     
 }
