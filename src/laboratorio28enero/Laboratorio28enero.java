2d213d0b24ebfcfc32bdfb7f8403a8789e76c7a6
BoroaDev7 <jmborjas@unitec.edu>
2022-01-28 16:08:42 -0600

    otro avance

--- src/laboratorio28enero/Laboratorio28enero.java
+++ src/laboratorio28enero/Laboratorio28enero.java
@@ -181,14 +181,7 @@ public class Laboratorio28enero {
              lista.remove(indice);
                    
     }
-     public static void alimentar(){
-        System.out.println("Ingrese la posicion del animal al que alimentara");
-        int pos=leer.nextInt();
-        System.out.println("Ingrese la posicion del animal al que se devoraran");
-        int pos2=leer.nextInt();
-        lista.get(pos).setVida((pos)+(pos2));
-        lista.remove(pos2);
-        
+     
     
     }
      
